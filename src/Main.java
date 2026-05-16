6c86aaa470e262a7a0d30db047395991bfe99a12
JohnFuly <johnatanfuly007@gmail.com>
2026-05-16 01:45:30 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
+import br.com.dio.desafio.dominio.Bootcamp;
+import br.com.dio.desafio.dominio.Conteudo;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -20,9 +25,46 @@ public class Main {
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devJohn = new Dev();
+        devJohn.setNome("John");
+        devJohn.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Incritos John" + devJohn.getConteudosInscritos());
+        devJohn.progredir();
+        devJohn.progredir();
+        devJohn.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Incritos John" + devJohn.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos John" + devJohn.getConteudosConcluidos());
+        System.out.println("XP" + devJohn.calcularTotalXP());
+
+        System.out.println("--------------------");
+
+
+        Dev devJohn2 = new Dev();
+        devJohn2.setNome("John2");
+        devJohn2.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Incritos John2" + devJohn2.getConteudosInscritos());
+        devJohn2.progredir();
+        devJohn2.progredir();
+        devJohn2.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Incritos John2" + devJohn2.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos John2" + devJohn2.getConteudosConcluidos());
+        System.out.println("XP" + devJohn2.calcularTotalXP());
+
+
+
 
     }
 }
